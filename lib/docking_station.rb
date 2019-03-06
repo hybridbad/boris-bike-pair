95a8190c2ac04ff94461762cb337c19f1d19049c
alexanderchen6 <alexbaggio315@hotmail.com>
2019-03-06 15:53:26 +0000

    11 finish, instance variable

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,12 @@
 class DockingStation
+	attr_reader :bike
+
   def release_bike
+  	Bike.new
   end
+
+  def docking(bike)
+  	@bike = bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,36 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to(:release_bike) }
 end
+
+describe DockingStation do 
+	it 'releases a working bike' do 
+		bike = Bike.new
+	expect(subject.release_bike) == bike
+	expect(bike.working?).to eq true 
+    # expect(bike).to be_working
+	end 
+end 
+
+describe DockingStation do 
+	it 'docks a bike at docking station, ' do 
+		expect(subject).to respond_to(:docking).with(1).argument
+	end 
+
+	it 'responds to bike' do 
+		is_expected.to respond_to(:bike)
+	end 
+
+	it 'docks something' do 
+		bike= Bike.new
+		expect(subject.docking(bike)).to eq bike 
+	end 
+
+	it 'returns docked bikes' do
+		bike = Bike.new
+		subject.docking(bike)
+		expect(subject.bike).to eq bike
+	end 
+
+end 
