62b1fec20f65f8a340f14a58123f2c1a06f4fc13
alexanderchen6 <alexbaggio315@hotmail.com>
2019-03-06 16:49:20 +0000

    finished releases bike error when start empty

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,11 +2,19 @@ class DockingStation
 	attr_reader :bike
 
   def release_bike
-  	Bike.new
+  	# if @bike != nil   
+	fail 'No available bikes' unless @bike
+	@bike
+
+
+
   end
 
   def docking(bike)
   	@bike = bike
   end
+
+
+
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,19 +1,21 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to(:release_bike) }
-end
+  	it { is_expected.to respond_to(:release_bike) }
 
-describe DockingStation do 
-	it 'releases a working bike' do 
-		bike = Bike.new
-	expect(subject.release_bike) == bike
-	expect(bike.working?).to eq true 
-    # expect(bike).to be_working
-	end 
-end 
 
-describe DockingStation do 
+	# it 'releases a working bike' do 
+	# # 	bike = Bike.new
+	# # expect(subject.release_bike) == bike
+	# # expect(bike.working?).to eq true 
+ #    # expect(bike).to be_working
+	# end 
+ 
+ #the test above was removed and the nested 'describe '#release bike'' test was added.
+ # we assumed the docking station starts empty (without any bikes).
+
+
+
 	it 'docks a bike at docking station, ' do 
 		expect(subject).to respond_to(:docking).with(1).argument
 	end 
@@ -33,4 +35,11 @@ describe DockingStation do
 		expect(subject.bike).to eq bike
 	end 
 
+
+	describe '#release_bike' do
+		it 'raises an error when docking station empty' do 
+			# subject.bike = nil 
+			expect { subject.release_bike }.to raise_error("No available bikes")	
+		end 
+	end
 end 
