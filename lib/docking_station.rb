3d207df47c5f4e876d03d66632b4e7445b9836b5
Riya Dattani <riya_dattani@hotmail.com>
2019-03-07 15:14:19 +0000

    finished 15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,28 +1,32 @@
-class DockingStation
-	attr_reader :bikes
+require './lib/bike.rb'
 
-	def initialize
-		@bikes = []
-	end 
+class DockingStation
+	attr_reader :docked, :capacity
+
+	def initialize(capacity = 20)
+		@docked = []
+		@capacity = capacity
+	end
 
   	def release_bike
-  	# if @bike != nil   
-	fail 'No available bikes' if @bikes.empty?
-	@bikes.pop
+  	# if @bike != nil
+	fail 'No available bikes' if @docked.empty?
+	@docked.pop
 	#releases the first bike
-
-
   	end
 
   def docking(bike)
-
-  fail 'Docking station full' if @bikes.count >= 20
-  
-  	@bikes << bike
-  	  
+    fail 'Docking station full' if full?
+  	@docked << bike
   end
 
 
+	def full?
+		@docked.count >= capacity
+	end
 
+	def empty?
+		@docked.empty?
+	end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,55 +4,60 @@ describe DockingStation do
   	it { is_expected.to respond_to(:release_bike) }
 
 
-	# it 'releases a working bike' do 
+	# it 'releases a working bike' do
 	# # 	bike = Bike.new
 	# # expect(subject.release_bike) == bike
-	# # expect(bike.working?).to eq true 
+	# # expect(bike.working?).to eq true
  #    # expect(bike).to be_working
-	# end 
- 
+	# end
+
  #the test above was removed and the nested 'describe '#release bike'' test was added.
  # we assumed the docking station starts empty (without any bikes).
 
 
 
-	it 'docks a bike at docking station, ' do 
+	it 'docks a bike at docking station, ' do
 		expect(subject).to respond_to(:docking).with(1).argument
-	end 
+	end
 
-	it 'responds to bike' do 
-		is_expected.to respond_to(:bikes)
-	end 
+	it 'responds to bike' do
+		is_expected.to respond_to(:docked)
+	end
 
-	it 'docks something' do 
+	it 'docks something' do
 		bike= Bike.new
-		expect(subject.docking(bike)).to eq [bike] 
-	end 
+		expect(subject.docking(bike)).to eq [bike]
+	end
 
 	it 'returns docked bikes' do
 		bike = Bike.new
 		subject.docking(bike)
-		expect(subject.bikes).to eq [bike]
-	end 
+		expect(subject.docked).to eq [bike]
+	end
 
 
 	describe '#release_bike' do
-		it 'raises an error when docking station empty' do 
-			# subject.bike = nil 
-			expect { subject.release_bike }.to raise_error("No available bikes")	
-		end 
+		it 'raises an error when docking station empty' do
+			# subject.bike = nil
+			expect { subject.release_bike }.to raise_error("No available bikes")
+		end
+    it 'returns true if docking station is empty' do
+      expect(subject.empty?).to eq true
+    end
 	end
 
-	describe '#docking' do 
-		it 'raises an error when docking station is full(1 bike)' do 
-			
-			20.times { subject.docking(Bike.new) }  
-
+	describe '#docking' do
+		it 'raises an error when docking station is full(1 bike)' do
+			20.times { subject.docking(Bike.new) }
+      #<< 20.times { subject.docking(Bike.new) }
 			expect { subject.docking(Bike.new) }.to raise_error("Docking station full")
-		end 
-	end 
+		end
+    it 'returns true when dock is full (more than 20)' do
+      20.times { subject.docking(Bike.new) }
+      expect(subject.full?).to eq true
+    end
 
-	
+	end
 
 
-end 
+end
